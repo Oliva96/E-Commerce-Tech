c5a4907cbc67bc98ec605a2cb18acd0c5ad13647
Oliva96 <oliva961206@gmail.com>
2022-05-11 15:45:23 -0400

    without Stripe

---
+++ components/Layout.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="layout">
+      <Head>
+        <title>Tech E-Commerce Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
